63bc7b6380d942a2d25e0f174381cb91143abe45
Ngọc Phú <65398386+ttpngocphu@users.noreply.github.com>
2024-10-08 10:49:56 +0700

    Update and rename full_star2qltechn.mk to full_dream2qltechn.mk

--- full_dream2qltechn.mk
+++ full_dream2qltechn.mk
@@ -1,12 +1,12 @@
 
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/star2qltechn/device.mk)
+$(call inherit-product, device/samsung/dream2qltechn/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := star2qltechn
-PRODUCT_NAME := full_star2qltechn
-PRODUCT_MODEL := SM-G9650
+PRODUCT_DEVICE := dream2qltechn
+PRODUCT_NAME := full_dream2qltechn
+PRODUCT_MODEL := SM-G9550
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
