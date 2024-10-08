1199415df6ffb78edbc96ab2ce918b71ba739e9f
Ngọc Phú <65398386+ttpngocphu@users.noreply.github.com>
2024-10-08 10:51:04 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,15 +1,15 @@
 
-# Inherit from samsung sdm845-common
--include device/samsung/sdm845-common/BoardConfigCommon.mk
+# Inherit from samsung sdm835-common
+-include device/samsung/sdm835-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/samsung/star2qltechn
+DEVICE_PATH := device/samsung/dream2qltechn
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := star2qltechn
+TARGET_OTA_ASSERT_DEVICE := dream2qltechn
 
 # Kernel
-TARGET_KERNEL_CONFIG := star2qlte_chn_open_defconfig
+TARGET_KERNEL_CONFIG := dream2qlte_chn_open_defconfig
 
 # Vendor init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_star2qltechn
-TARGET_RECOVERY_DEVICE_MODULES := libinit_star2qltechn
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_dream2qltechn
+TARGET_RECOVERY_DEVICE_MODULES := libinit_dream2qltechn
