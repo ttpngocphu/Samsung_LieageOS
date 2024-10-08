fc65cfbc7f6ae27b533baa75b52433b91a8217d7
Ngọc Phú <65398386+ttpngocphu@users.noreply.github.com>
2024-10-08 10:50:15 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,5 +4,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Inherit common device configuration
-$(call inherit-product, device/samsung/sdm845-common/common.mk)
+$(call inherit-product, device/samsung/sdm835-common/common.mk)
 
